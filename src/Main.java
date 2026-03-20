199055a23ac4c8661c686548715b30e98c882808
ricardo <ricardo.alvear.personal@gmail.com>
2026-03-20 17:16:32 -0400

    added more validation checks and determined the win or draw conditions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Objects;
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
 
     // Imported ANSI escape codes to change the text color of the console
     static Scanner scanner = new Scanner(System.in);
@@ -11,7 +12,8 @@ public class Main {
     public static final String YELLOW = "\u001B[33m";
     public static final String BLUE = "\u001B[34m";
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         // Name of members and ids:
         // Ricardo Alvear | 101536306
@@ -23,14 +25,16 @@ public class Main {
 
         // While the user input does not equal to "1" or "2",
         // Continue asking them for the valid input
-        while(!Objects.equals(choice, "1") && !Objects.equals(choice, "2")){
+        while(!Objects.equals(choice, "1") && !Objects.equals(choice, "2"))
+        {
             System.out.println(RED + "\nSorry, invalid input." + RESET);
             System.out.println("Please enter 1 for single player or 2 for two players: ");
             choice = scanner.nextLine();
         }
 
         // Selects the correct method depending on the user input
-        switch (choice){
+        switch (choice)
+        {
             case "1":
                 System.out.println(YELLOW + "\nNow loading single player game..." + RESET);
                 OnePlayer();
@@ -39,14 +43,16 @@ public class Main {
                 System.out.println(YELLOW + "\nNow loading two player game..." + RESET);
                 TwoPlayer();
         }
+
     }
 
-    public static void OnePlayer(){
+    public static void OnePlayer()
+    {
           System.out.println(GREEN + "Starting the game!" + RESET);
 //        Sayuni will do this:
-//        2) If the user selects 1 player (versus minimax AI)
+//        2) If the user selects 1 player (versus Minimax AI)
 //        a. The Human player should be asked their name
-//        b. The player must be prompted to choose their symbol (‘X’ or ‘O’)
+//        b. The player must be prompted to choose their symbol (‘X’ or O’)
 //        c. The player with symbol ‘X’ should go first (computer or human player).
 //        d. The present board state will be displayed and:
 //        1. If the computer is to play, it will now show the board updated with the
@@ -58,71 +64,175 @@ public class Main {
 
     }
 
-    public static void TwoPlayer(){
-
-        // RICARDO WILL DO THIS
-
-        // 3) If the user selects 2 player (2 Human players)
-        //a. Each human player must be prompted to choose their name and symbol (‘X’ or ‘O’)
-        //b. An empty board will then be displayed.
-        //c. The next player to move will be prompted for a row and column location to play.
-        //d. The present board state will be displayed showing their updated play.
-        //e. The board will then be checked for a winning state or draw and if it is not in a winning
-        //state or draw, go to step c otherwise display the winner or declare a draw
+    public static void TwoPlayer()
+    {
 
         System.out.println(GREEN + "Starting the game!\n" + RESET);
         String winner = "";
+        String p1Symbol;
+        String p2Symbol = "";
         boolean draw = false;
 
         System.out.print("Player 1 | Please enter your name: ");
         String p1 = scanner.nextLine();
 
-        while (!p1.matches("^[a-zA-Z]+$") ){
-            System.out.println(RED + "\nSorry, invalid input." + RESET);
-            System.out.print("Please enter your name: ");
-            p1 = scanner.nextLine();
-
-        }
+        nameCheckTwoPlayerGame(p1);
 
         System.out.print("Please enter your symbol: ");
-        String p1Symbol = scanner.nextLine().toLowerCase();
+        p1Symbol = scanner.nextLine().toLowerCase();
 
-        // While the input for 'p1' does not contain only letters,
-        // Continue asking for the valid input
-        while (!Objects.equals(p1Symbol, "o")  && !Objects.equals(p1Symbol, "x")){
-            System.out.println(RED + "\nSorry, invalid input." + RESET);
-            System.out.print("Please enter your symbol: ");
-            p1Symbol = scanner.nextLine();
-        }
+        symbolCheckTwoPlayerGame(p1Symbol, p2Symbol);
 
         System.out.print("Player 2 | Please enter your name: ");
         String p2 = scanner.nextLine();
 
-        // While the input for 'p2' does not contain only letters,
-        // Continue asking for the valid input
-        while (!p2.matches("^[a-zA-Z]+$") ){
-            System.out.println(RED + "\nSorry, invalid input." + RESET);
-            System.out.print("Please enter your name: ");
-            p2 = scanner.nextLine();
-
-        }
+        nameCheckTwoPlayerGame(p2);
 
         System.out.print("Please enter your symbol: ");
-        String p2Symbol = scanner.nextLine().toLowerCase();;
+        p2Symbol = scanner.nextLine().toLowerCase();
 
-        // While the input for 'p2Symbol' does not contain either X or O,
-        // Continue asking for the valid input
-        while (!Objects.equals(p2Symbol, "o")  && !Objects.equals(p2Symbol, "x")){
-            System.out.println(RED + "\nSorry, invalid input." + RESET);
-            System.out.print("Please enter your symbol: ");
-            p2Symbol = scanner.nextLine();
-        }
+        symbolCheckTwoPlayerGame(p1Symbol, p2Symbol);
 
         // While the winner is not either player one or player two or a draw has not been reached,
         // Continue the game
-        while (!Objects.equals(winner, p1) || !Objects.equals(winner, p2) || !Objects.equals(draw, true)){
+        while (!Objects.equals(winner, p1) || !Objects.equals(winner, p2) || !Objects.equals(draw, true))
+        {
+
+            // Make an array of integers that has three columns and rows
+            int[][] array = new int[3][3];
+
+            System.out.println("Player 1 | Enter the column and row (comma separated):");
+
+            columnsRowsCheckTwoPlayerGame(winner, draw, p1Symbol, array);
+
+            System.out.println("Player 2 | Enter the column and row (comma separated):");
+
+            columnsRowsCheckTwoPlayerGame(winner, draw, p2Symbol, array);
+
+
+            // Check if the result of the method matches one of the conditions,
+            // If true, break out of the while loop
+            if (columnsRowsCheckTwoPlayerGame(winner, draw, p1Symbol, array).equals("Player 1"))
+            {
+                winner = p1;
+            }else if(columnsRowsCheckTwoPlayerGame(winner, draw, p2Symbol, array).equals("Player 2"))
+            {
+                winner = p2;
+            }else if (columnsRowsCheckTwoPlayerGame(winner, draw, p1Symbol, array).equals("draw") && columnsRowsCheckTwoPlayerGame(winner, draw, p2Symbol, array).equals("draw"))
+            {
+                draw = true;
+            }
+
+        }
+
+        if (!Objects.equals(winner, ""))
+        {
+            System.out.println(GREEN + "CONGRATULATIONS! " + winner + " has won the game!" + RESET);
+        }
+        else if (draw)
+        {
+            System.out.println(GREEN + "DRAW! " + p1 + " and " + p2 + " have tied" + RESET);
+        }
+
+
+    }
+
+    public static String columnsRowsCheckTwoPlayerGame(String winner, boolean draw, String curPlayer, int[][] array)
+    {
+        while (true)
+        {
+
+            String[] data;
+            try
+            {
+
+                data = scanner.nextLine().split(",");
+
+                // Try to parse the columns and rows
+                int p2Column = Integer.parseInt(data[0].trim());
+                int p2Row = Integer.parseInt(data[1].trim());
+
+                // If the player has the symbol 'x', pass that symbol which uniquely identifies that player
+                // Otherwise, pass the symbol that uniquely identifies the second player
+                // Regardless, the array with the indicated column and row will be passed
+                if (Objects.equals(curPlayer, "x")){
+                    return tablePrintAndCheckResult("x", array[p2Column][p2Row]);
+                }else{
+                    return tablePrintAndCheckResult("o", array[p2Column][p2Row]);
+                }
+            } catch (Exception e)
+            {
+                System.out.println(RED + "\nSorry, invalid input" + RESET);
+                System.out.println("\nPlease, enter a valid input");
+                columnsRowsCheckTwoPlayerGame(winner, draw, curPlayer, array);
+            }
 
         }
     }
-}
 
+    public static String tablePrintAndCheckResult(String symbol, int array)
+    {
+
+        String result = "";
+
+        // Print the table with the current placements of all the symbols
+        System.out.println(
+                BLUE + "   Table\n" + RESET +
+                "   |   " +    "|\n" +
+                "---" + "|---" + "|" + "---\n" +
+                "---" + "|---" + "|" + "---\n" +
+                "   |   " + "|"
+        );
+
+        // Check if the result is one of the players or a draw
+        if (Objects.equals(result, "Player 1"))
+        {
+            return result;
+        }else if (Objects.equals(result, "Player 2"))
+        {
+            return result;
+        }else if (result.equals("draw"))
+        {
+            return result;
+        }
+
+        return "Continuing";
+    }
+
+    public static void nameCheckTwoPlayerGame(String name)
+    {
+
+        // While the input does not contain only letters,
+        // Continue asking for the valid input
+        while (!name.matches("^[a-zA-Z]+$"))
+        {
+            System.out.println(RED + "\nSorry, invalid input." + RESET);
+            System.out.print("Please enter your name: ");
+            name = scanner.nextLine();
+        }
+
+    }
+
+    public static void symbolCheckTwoPlayerGame(String p1Symbol, String p2Symbol)
+    {
+
+        // While the symbols are the same,
+        // continue asking for the valid input
+        while (Objects.equals(p1Symbol, p2Symbol))
+        {
+            System.out.println(RED + "\nSorry, invalid input." + RESET);
+            System.out.print("Cannot be the same. enter your symbol: ");
+            p2Symbol = scanner.nextLine().toLowerCase();
+        }
+
+        // While symbols do not contain either 'x' or 'o',
+        // continue asking for the valid input
+        while (!Objects.equals(p1Symbol, "o")  && !Objects.equals(p1Symbol, "x") && !Objects.equals(p2Symbol, "o")  && !Objects.equals(p2Symbol, "x"))
+        {
+            System.out.println(RED + "\nSorry, invalid input." + RESET);
+            System.out.print("Please enter your symbol: ");
+            p2Symbol = scanner.nextLine().toLowerCase();
+        }
+
+    }
+}
